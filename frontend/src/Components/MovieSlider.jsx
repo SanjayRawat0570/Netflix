686ff2a5b3afea09dd8bcc2fec666a3b86c0a9a8
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-02-02 21:08:47 +0530

    initial commit

--- frontend/src/Components/MovieSlider.jsx
+++ frontend/src/Components/MovieSlider.jsx
@@ -1,14 +1,42 @@
-import {useContentstore} from '../store/Contentstore'
-
+import {useContentstore} from '../store/Contentstore';
 const  MovieSlider =({category})=> {
-    console.log(category);
+    const {contentType} = useContentstore();
+    const [content, setCount] = useState([]);
+    const [showArrows, setShowArrows] = useState(false);
+    const formattedCategoryName  = category.replaceAll("-", " ") [0].toUpperCase() + category.replaceAll("-", " ").slice(1);
+    const formattedType = contentType === 'movie' ? 'Movies' : 'TV Shows';
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const getContent = async () => {
+            const res = await axios.get(`/api/v1/${contentType}/${category}`);
+            setMovies(res.data.movies);
+        }
+      getContent()
+    }, [ contentType, category]);
   return (
-    <div classname ="bg-black text-white relative px-5 md:px-20">
-        <h2>
-            {category} {contentType}
+    <div className= "bg-black text-white relative px-5 md:px-20">
+        <h2 className = 'mb-4 text-2xl font-bold'>
+            {formattedCategoryName} {formattedContentType}
         </h2>
-    </div>>
-  )
-}
+        <div className= 'flex space-x-4 overflow-x-scroll'>
+            {contentType.map((item)=>(
+                <Link to={'/watch/${item.id}'} className = 'min-w-[250px] relative group' key= { item.id}>
+                    <div className = 'rounded-lg overflow-hidden'>
+                        <img 
+                        src= {SMALL-IMG_BASE_URL + item.backdrop_path}
+                        alt = 'Movie image'
+                        className = 'transition-transform duration-300 ease-in-out group-hover: scale-125'/>
+                    </div>
+                    <p className = 'mt-2 text-center'>
+                        {item.title || item.name}
+                        </p>
+                        
+    </Link>
+    
+    ))}
+    </div>
+    </div>
+    );
+};
 
 export default MovieSlider
